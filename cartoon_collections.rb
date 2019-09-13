e947cf050cf29f3e5ca387f8a6acd4b3603b5cac
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-13 20:35:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -34,4 +34,9 @@ detect
 def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
+  strings.detect do {
 end
+
+
+[1,2,3,4].detect{|i| i.even?} #=> 2
+[1,2,3,4].detect{|i| i.is_a?(String)} #=> nil
